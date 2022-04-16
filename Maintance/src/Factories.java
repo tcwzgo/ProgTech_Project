427a3990f991f541e84207b3d73a9f4def18c892
krisztian <hegyikrisztian1@gmail.com>
2022-04-16 18:01:00 +0200

    factories window styled

--- Maintance/src/Factories.java
+++ Maintance/src/Factories.java
@@ -2,6 +2,14 @@ import javax.swing.*;
 
 public class Factories {
     private JPanel Main;
+    private JTextField tfCompanyName;
+    private JTextField tfInstitution;
+    private JTextField tfAddress;
+    private JButton btnAdd;
+    private JButton btnSearch;
+    private JButton btnDelete;
+    private JButton btnUpdate;
+    private JTable dataTable;
 
 
     public static void main(String[] args) {
@@ -15,5 +23,7 @@ public class Factories {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
+        frame.setResizable(false);
+        frame.setSize(600, 400);
     }
 }
