c91d63ff98354d8e075925ec3e480432e1440337
Milán <suhaj73@gmail.com>
2022-04-16 18:41:17 +0200

    Employees window styled.

--- Maintance/src/Employees.java
+++ Maintance/src/Employees.java
@@ -1,7 +1,42 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Employees {
     private JPanel Main;
+    private JLabel nameLabel;
+    private JLabel emailLabel;
+    private JLabel postLabel;
+    private JLabel salaryLabel;
+    private JTextField nameText;
+    private JTextField emailText;
+    private JTextField postText;
+    private JTextField salaryText;
+    private JButton addBtn;
+    private JTable table1;
+    private JButton deleteBtn;
+    private JButton updateBtn;
+
+    public Employees() {
+        addBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        deleteBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        updateBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                
+            }
+        });
+    }
 
     public static void main(String[] args) {
         showWindow();
@@ -14,5 +49,7 @@ public class Employees {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
+        frame.setResizable(false);
+        frame.setSize(600, 400);
     }
 }
