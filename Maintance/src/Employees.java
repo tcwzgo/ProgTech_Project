e2f527fdb60dab48a52f209f0bbfcb66c8b09c3a
Milán <suhaj73@gmail.com>
2022-04-17 16:51:23 +0200

    The function of Add button on Employee form is done.

--- Maintance/src/Employees.java
+++ Maintance/src/Employees.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
@@ -18,7 +19,7 @@ public class Employees {
     private JTextField postText;
     private JTextField salaryText;
     private JButton addBtn;
-    private JTable table1;
+    private JTable table;
     private JButton deleteBtn;
     private JButton updateBtn;
 
@@ -28,6 +29,33 @@ public class Employees {
         addBtn.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+
+                String name, email, post, salary;
+                name = nameText.getText();
+                email = emailText.getText();
+                post = postText.getText();
+                salary = salaryText.getText();
+
+                try {
+                    DBConnect dbcon = new DBConnect();
+                    Connection connection = dbcon.Connect("maintance");
+                    PreparedStatement pst = connection.prepareStatement("insert into employees (name, email, post, salary) values(?, ?, ?, ?)");
+                    pst.setString(1, name);
+                    pst.setString(2, email);
+                    pst.setString(3, post);
+                    pst.setString(4, salary);
+                    pst.executeUpdate();
+                    JOptionPane.showMessageDialog(null, "Record added!");
+                    logger.info("Record added to employees table succesfully.");
+                    nameText.setText("");
+                    emailText.setText("");
+                    postText.setText("");
+                    salaryText.setText("");
+                } catch(Exception ex){
+                    ex.printStackTrace();
+                    JOptionPane.showMessageDialog(null, "Failed adding record!");
+                    logger.info("Adding record to employees table was unsuccessful.");
+                }
 
             }
         });
