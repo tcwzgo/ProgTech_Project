4265328cf59fac911a191692ccd349d288591c19
Milán <suhaj73@gmail.com>
2022-04-19 18:51:09 +0200

    Implemented EmployeeClass class.

---
+++ Maintance/src/Observer/EmployeeClass.java
@@ -0,0 +1,125 @@
+package Observer;
+
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmployeeClass {
+    private String name;
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String n)
+    {
+        if (n.length() < 5)
+        {
+            JOptionPane.showMessageDialog(null, "The length of the name must be at least five character!", "Warning!", JOptionPane.WARNING_MESSAGE);
+        }
+        else
+        {
+            this.name = n;
+        }
+    }
+
+    private String email;
+
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public void setEmail(String e)
+    {
+        if (e.length() < 10)
+        {
+            JOptionPane.showMessageDialog(null, "The length of the email must be at least ten character!", "Warning!", JOptionPane.WARNING_MESSAGE);
+        }
+        else if (!e.contains("@"))
+        {
+            JOptionPane.showMessageDialog(null, "Email must contain '@'!", "Warning!", JOptionPane.WARNING_MESSAGE);
+        }
+        else{
+            this.email = email;
+        }
+    }
+
+    private String post;
+
+    public String getPost()
+    {
+        return post;
+    }
+
+    public void setPost(String p)
+    {
+        if (p.length() < 3)
+        {
+            JOptionPane.showMessageDialog(null, "The length of the post must be at least three characters!", "Warning!", JOptionPane.WARNING_MESSAGE);
+        }
+        else
+        {
+            this.email = email;
+        }
+    }
+
+    private int salary;
+
+    public int getSalary()
+    {
+        return salary;
+    }
+
+    public void setSalary(int s)
+    {
+        if (s < 120000 || s > 1000000)
+        {
+            JOptionPane.showMessageDialog(null, "The salary must be in [120000;1000000] range!", "Warning!", JOptionPane.WARNING_MESSAGE);
+        }
+        else
+        {
+            this.salary = salary;
+            notifyAllObservers();
+        }
+    }
+
+    public EmployeeClass(String name, String email, String post, int salary)
+    {
+        this.name = name;
+        this.email = email;
+        this.post = post;
+        this.salary = salary;
+    }
+
+    private List<Observer> observers = new ArrayList<Observer>();
+
+    public void notifyAllObservers(){
+        for (Observer observer : observers) {
+            observer.sendMessage();
+        }
+    }
+
+    public void addObserver(Observer observer){
+        observers.add(observer);
+    }
+
+    private ArrayList<EmployeeClass> employees = new ArrayList<EmployeeClass>();
+
+    public void AddEmployee(EmployeeClass e)
+    {
+        if (!e.equals(null))
+        {
+            employees.add(e);
+        }
+    }
+
+    public void ListEmployees()
+    {
+        for(int i = 0; i < employees.size(); i++)
+        {
+            System.out.println(employees.get(i).getName() + " " + employees.get(i).getEmail() + " " + employees.get(i).getPost()+ " " + employees.get(i).getSalary()) ;
+        }
+    }
+}
