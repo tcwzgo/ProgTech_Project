31cd7cc534b96ad5d34a4341ed224db5eacc426d
krisztian <hegyikrisztian1@gmail.com>
2022-05-01 20:32:53 +0200

    action events added on Factories

--- Maintance/src/Factories.java
+++ Maintance/src/Factories.java
@@ -2,6 +2,8 @@ import Proxy.FactoryProxy;
 import org.apache.log4j.Logger;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Factories {
     private JPanel Main;
@@ -21,6 +23,34 @@ public class Factories {
     private static Logger logger = Logger.getLogger(Employees.class);
     private static DBConnect database = new DBConnect("maintance");
     private FactoryProxy factoryProxy;
+
+    public Factories() {
+
+        btnAdd.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        btnDelete.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        btnSearch.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        btnUpdate.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
 
     public static void showWindow()
     {
